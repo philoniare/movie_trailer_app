0bf60cc7d0ab5e2edc48b9f22111ced2f2fd3db0
philoniare <philoniare@gmail.com>
2016-01-25 20:50:40 -0500

    Initial commit

---
+++ media.py
@@ -0,0 +1,11 @@
+import webbrowser
+
+class Movie():
+    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.image = poster_image
+        self.trailer = trailer_youtube
+    def show_trailer(self):
+        webbrowser.open(self.trailer)
+        return self.trailer
